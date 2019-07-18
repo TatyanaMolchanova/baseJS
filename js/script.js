86cb572bae6ca1b30ee73c822e51a5520e205b21
Molchanova Tatyana <mtuspeh@gmail.com>
2019-07-18 17:42:03 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+alert('Я люблю БОРЩ!');
+
+let admin;
+let name;
+name = 'Василий';
+admin = name;
+console.log(admin);
+
+let ourPlanet = 'Земля';
+let login = 'Петя';
+
+var username = prompt('What is your name?');
+alert(username);
+
